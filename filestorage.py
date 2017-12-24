b5d12383241cb01d11ee84ea3a362725aa1ee0c6
Pavel Liferenko <2kie.co@gmail.com>
2017-12-24 16:53:34 +0000

    unneccessary comment in code

--- filestorage.py
+++ filestorage.py
@@ -5,7 +5,6 @@ from view.simple_page import simple_page
 
 app = Flask(__name__)
 app.register_blueprint(simple_page)
-# Blueprint can be registered many times
 app.register_blueprint(simple_page, url_prefix='/pages')
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
