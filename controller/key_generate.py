2f0d4a7925d7716c3490d1f09705c26f2b43267e
Pavel Liferenko <2kie.co@gmail.com>
2017-12-24 19:07:45 +0000

    a

--- controller/key_generate.py
+++ controller/key_generate.py
@@ -1,2 +1,9 @@
+from itsdangerous import URLSafeSerializer
+s = URLSafeSerializer('secret-key')
+s.dumps([1, 2, 3, 4])
+
+s.loads('WzEsMiwzLDRd.wSPHqC0gR7VUqivlSukJ0IeTDgo')
+[1, 1]
+
         #генерируется уникальный ключ доступа к файлу
-        .generate_new_file_identification_key()
+        # .generate_new_file_identification_key()
