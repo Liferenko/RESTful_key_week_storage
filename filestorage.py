f51a02dc581e897f0f79d5746b84037cf4f13be3
Liferenko <2kie.co@gmail.com>
2017-12-27 11:39:05 +0000

    some cleaning just for good looking

--- filestorage.py
+++ filestorage.py
@@ -5,6 +5,7 @@ from view.simple_page import simple_page
 
 app = Flask(__name__)
     
+    
 app.register_blueprint(simple_page)
 app.register_blueprint(simple_page, url_prefix='/pages')
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
