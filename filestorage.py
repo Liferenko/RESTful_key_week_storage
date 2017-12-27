fb7634472af019fc8c6e7c3c95e005ce1ac4e722
Pavel Liferenko <2kie.co@gmail.com>
2017-12-27 11:38:04 +0000

    some cleaning just for good looking

--- filestorage.py
+++ filestorage.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(simple_page)
 app.register_blueprint(simple_page, url_prefix='/pages')
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-
-    
+# getenv only for cloud9 environment
 
 if __name__=='__main__':
   app.run(debug=True)
